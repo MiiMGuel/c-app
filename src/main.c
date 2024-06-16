7171e6127a6d6003eb9991114f4066521e5ea12b
migel <miigel065@gmail.com>
2024-06-16 16:46:53 +0700

    Update, App version

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "app_config.h"
 
 int main(void) {
-    printf("Hello, World!");
+    printf("Hello, World!\n");
+    printf("version : %d.%d.%d.%d\n", 
+        App_VERSION_MAJOR, 
+        App_VERSION_MINOR, 
+        App_VERSION_PATCH, 
+        App_VERSION_TWEAK
+    );
     
     return 0;
 }
